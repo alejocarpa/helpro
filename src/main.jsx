569b34fab4bfb0350ea67a089a4ebfd36cfc49d5
Alejandro Cardenas <alejocarpa@hotmail.com>
2024-03-19 16:17:09 -0500

    Se coloco HashRouter para que cargara bien las rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { HelproApp } from './HelproApp';
 import './index.css';
 import { store } from './store';
@@ -9,9 +9,9 @@ import { store } from './store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={ store }>
-    <BrowserRouter>
+    <Router>
       <HelproApp />
-    </BrowserRouter>
+    </Router>
   </Provider>
   // </React.StrictMode>,
 )
