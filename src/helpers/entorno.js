ca63952908ba15623ef59fab488c696b87542a99
Alejandro Cardenas <alejocarpa@hotmail.com>
2024-07-16 07:55:49 -0500

    Se modifico la ruta de las imagenes

--- src/helpers/entorno.js
+++ src/helpers/entorno.js
@@ -3,4 +3,5 @@ export const urlEndpoint = 'https://proyectoeasy.net/helpro';
 
 export const apikeyEndpoint = '8SGTbDrCxv6AuKbavVq5XY9Hb3GXPv';
 
-export const urlEndpointImages = 'https://proyectoeasy.net/helpro/imagesServer';
+//export const urlEndpointImages = 'https://proyectoeasy.net/helpro/imagesServer';
+export const urlEndpointImages = 'https://proyectoeasy.net/imagesHelpro';
