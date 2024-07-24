8705f1988d92c94d37abe3c866e5d25ba1fb9d4e
Alejandro Cardenas <alejocarpa@hotmail.com>
2024-07-24 10:54:45 -0500

    Se modifico el banner del home

--- src/helpro/components/CarouselComponent.jsx
+++ src/helpro/components/CarouselComponent.jsx
@@ -14,8 +14,8 @@ export const CarouselComponent = () => {
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h1>Objetivo</h1>
+                    <p style={{ fontWeight: '400', fontSize: 25 }}>Entre todos podamos crear una gran base de datos critica, destacando lo bueno y lo malo de cada producto o servicio</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={5000}>
@@ -25,8 +25,8 @@ export const CarouselComponent = () => {
                     alt="Second slide"
                 />
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h1>¿Como?</h1>
+                    <p style={{ fontWeight: '400', fontSize: 25 }}>Deja tu comentario y calificación sobre un producto que hayas comprado o hayas probado</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={5000}>
@@ -36,9 +36,9 @@ export const CarouselComponent = () => {
                     alt="Third slide"
                 />
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    <h1>Juntos hacemos la diferencia</h1>
+                    <p style={{ fontWeight: '400', fontSize: 25 }}>
+                        Los comentarios nos ayudaran a tomar la mejor decisión
                     </p>
                 </Carousel.Caption>
             </Carousel.Item>
